5270afb6e8fc2cc85461c65adcc6a0f5d5ca18d7
Mior Muhammad Zaki <crynobone@gmail.com>
2021-06-17 19:04:27 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/nova.php
+++ src/nova.php
@@ -7,6 +7,38 @@ use Laravel\Nova\Http\Requests\ActionRequest;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
 /**
+ * Determine Resource ID from the request.
+ *
+ * @param  callable|null  $default
+ *
+ * @return int|string|null
+ */
+function request_resource(NovaRequest $request, string $resource, $default = null)
+{
+    if ($request->resource === $resource && ! \is_null($request->resourceId)) {
+        return $request->resource;
+    }
+
+    return value($default);
+}
+
+/**
+ * Determine attachable Resource ID from the request.
+ *
+ * @param  callable|null  $default
+ *
+ * @return int|string|null
+ */
+function request_attachable_resource(NovaRequest $request, string $resource, $default = null)
+{
+    if ($request->relatedResource === $resource && ! \is_null($request->relatedResourceId)) {
+        return $request->relatedResourceId;
+    }
+
+    return request_resource($request, $resource, $default);
+}
+
+/**
  * Determine running action request.
  */
 function running_action(Request $request, ?string $action = null): bool
