2a49ba4b7032df2e2f822e6607ed9ddcf709a5d0
Mior Muhammad Zaki <crynobone@gmail.com>
2022-06-28 12:19:32 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ tests/Feature/IsRtlTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace NovaKit\Tests\Feature;
+
+use Illuminate\Foundation\Auth\User;
+use function NovaKit\is_rtl;
+use Orchestra\Testbench\TestCase;
+
+/**
+ * @testdox NovaKit\is_rtl() tests
+ */
+class IsRtlTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider localeDataProvider
+     */
+    public function it_can_detect_locale($locale, $direction)
+    {
+        app()->setLocale($locale);
+
+        $this->assertSame($direction, is_rtl() ? 'rtl' : 'ltr');
+    }
+
+    public function localeDataProvider()
+    {
+        yield ['en', 'ltr'];
+        yield ['ms', 'ltr'];
+        yield ['ar', 'rtl'];
+        yield ['ckb', 'rtl'];
+        yield ['uz_AF', 'rtl'];
+    }
+}
